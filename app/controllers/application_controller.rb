2c264998def7b7761bb8ac3ce6ed232cea1c7590
fxhover <736698959@qq.com>
2014-08-25 13:02:29 +0800

    set 404 and 500 error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Exception, with: :error_500
+  rescue_from ActiveRecord::RecordNotFound, with: :error_404
+
+  def error_500
+    render file: File.join(Rails.root, 'public/500.html'), status: 500, layout: false
+  end
+
+  def error_404
+    render file: File.join(Rails.root, 'public/404.html'), status: 404, layout: false
+  end
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class BlogsController < ApplicationController
   def index
-
+    render text: 'welcome.'
   end
 
   def new
