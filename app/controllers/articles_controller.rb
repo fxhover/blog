8a2852a92e110c14d2a2694254f3f8fffff05b94
fxhover <736698959@qq.com>
2014-08-26 19:46:47 +0800

    articles list paginate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,11 @@ class ArticlesController < ApplicationController
   before_filter :check_current_user_is_admin, only: [:new, :create, :edit, :update]
   before_filter :article, only: [:show, :edit, :update, :destroy]
 
+  def index
+    res = Article
+    @articles = res.order('created_at desc').page(params[:page]).per(2)
+  end
+
   def new
     @article = Article.new
   end
