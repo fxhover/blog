ee518b7c39060679e03ed114a6d190b02e2bc6fc
fxhover <736698959@qq.com>
2014-08-28 10:22:40 +0800

    edit comment and post comment change use ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,41 @@
 class CommentsController < ApplicationController
   before_filter :article, only: [:create, :edit, :update, :destroy]
   before_filter :require_login
-  before_filter :get_comment, only: [:update, :destroy]
+  before_filter :get_comment, only: [:edit, :update, :destroy]
 
   def create
+    @result = {status: true, message: ''}
     @comment = @article.comments.new content: params[:article_comment][:content]
     @comment.user_id = @current_user.id
-    unless @comment.save
-      flash[:error] = '评论失败'
+    if @comment.save
+      @result[:status] = true
+    else
+      @result[:message] = '发表评论失败'
     end
-    redirect_to article_path(@article)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def edit
+    respond_to do |format|
+      format.js
+    end
   end
 
   def update
-
+    @result = {status: false, message: ''}
+    if params[:article_comment].present? && params[:article_comment][:content].present?
+      @comment.content = params[:article_comment][:content]
+      if @comment.save
+        @result[:status] = true
+      else
+        @result[:message] = '编辑评论失败'
+      end
+    end
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
