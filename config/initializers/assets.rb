9c0fbaf316e818c7e0f94f6b72279f722c968abf
fxhover <736698959@qq.com>
2014-09-04 09:18:18 +0800

    some config change

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,19 +11,19 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( register.css *.png)
 
-# Rails.application.config.assets.precompile << Proc.new { |path|
-#   if path =~ /\.(css|js|png)\z/
-#     full_path = Rails.application.assets.resolve(path).to_path
-#     app_assets_path = Rails.root.join('app', 'assets').to_path
-#     vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
-#     if full_path.starts_with? app_assets_path or full_path.starts_with? vendor_assets_path
-#       puts "including asset: " + full_path
-#       true
-#     else
-#       puts "excluding asset: " + full_path
-#       false
-#     end
-#   else
-#     false
-#   end
-# }
+Rails.application.config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(css|js|png)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
+    if full_path.starts_with? app_assets_path or full_path.starts_with? vendor_assets_path
+      puts "including asset: " + full_path
+      true
+    else
+      puts "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+}
